d4a6a9172c5f1fdf6b508258dfe390c19fec69db
Kumar saurav <kumarsaurav020@gmail.com>
2017-03-30 22:42:28 +0530

    helper fnc

--- ui/src/main/java/com/graph/helper.java
+++ ui/src/main/java/com/graph/helper.java
@@ -1,15 +1,14 @@
 package com.graph;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.graph.beans.HashTag;
 import com.graph.beans.Message;
 import com.graph.beans.ProfileUser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +21,9 @@ import java.util.List;
  */
 public class Helper {
 
-    public static List<Message> getMessages() throws FileNotFoundException {
+    public static List<Message> getMessages(int i) throws IOException {
 
-        JsonArray array = getJsonArray();
+        JsonArray array = getJsonArray(i);
 
         System.out.println("Total message in file: " + array.size());
 
@@ -38,7 +37,17 @@ public class Helper {
             }
         }
         System.out.println("Total Message received: " + messages.size());
+
+        saveToFile(messages);
+
         return messages;
+    }
+
+    private static void saveToFile(List<Message> messages) throws IOException {
+        Gson gson = new Gson();
+        FileWriter fileWriter = new FileWriter("/Users/saurav/src/GraphJet2/nextTime.txt");
+        fileWriter.write(gson.toJson(messages));
+        fileWriter.close();
     }
 
     private static Message convertToMessage(JsonObject sourceMap) {
@@ -76,20 +85,21 @@ public class Helper {
         return null;
     }
 
-    public static JsonArray getJsonArray() throws FileNotFoundException {
-        //return type2();
-        return type1();
+    public static JsonArray getJsonArray(int i) throws FileNotFoundException {
+        return type2(i);
+        //return type1();
     }
 
-    public static JsonArray type2() throws FileNotFoundException {
+    public static JsonArray type2(int i) throws FileNotFoundException {
         JsonParser parser = new JsonParser();
-        JsonElement a = parser.parse(new FileReader("/Users/saurav/Downloads/UM.json"));
+        JsonElement a = parser.parse(new FileReader("/Users/saurav/Downloads/hits" + i));
         return a.getAsJsonArray();
     }
 
     public static JsonArray type1() throws FileNotFoundException {
         JsonParser parser = new JsonParser();
-        JsonElement a = parser.parse(new FileReader("/Users/saurav/src/G.json"));
+        //JsonElement a = parser.parse(new FileReader("/Users/saurav/src/GraphJet2/UM_1.json"));
+        JsonElement a = parser.parse(new FileReader("/Users/saurav/src/GraphJet2/output.txt"));
         return a.getAsJsonObject().get("hits").getAsJsonObject().get("hits").getAsJsonArray();
     }
 
